de050f910990e820e26fbd280a42e5418553543b
LewisJPaul <lewis.jp@gmail.com>
2014-10-23 23:03:56 -0400

    Initial Commit

---
+++ LCTHW/LCTHW/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  LCTHW
+//
+//  Created by Jeremy Lewis on 10/23/14.
+//  Copyright (c) 2014 LCTHW. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
