c5a1005800baf2fe08c9a56c28ab60e2c776b0ce
LewisJPaul <lewis.jp@gmail.com>
2014-10-25 12:51:17 -0400

    Exercise 5: The Structure Of A C Program

--- LCTHW/LCTHW/main.c
+++ LCTHW/LCTHW/main.c
@@ -1,36 +1,36 @@
-//
-//  main.c
-//  LCTHW
-//
-//  Created by Jeremy Lewis on 10/23/14.
-//  Copyright (c) 2014 LCTHW. All rights reserved.
-//
+/*
+ Quick review of C
+ 
+ Covers Exercises 1 & 2
+ http://www.codingunit.com/printf-format-specifiers-format-conversions-and-formatted-output
+ 
+ Quick exercises
+ http://c.learncodethehardway.org/
+ 
+ 
+*/
 
+// import the contents of one file into this source file.
+// h extensions for "header" files
 #include <stdio.h>
 
-int main()
+
+/*
+ argv and argc are how command line arguments are passed to main() in C and C++.
+ 
+ argc will be the number of strings pointed to by argv. This will (in practice) be 1 plus the number of arguments, as virtually all implementations will prepend the name of the program to the array.
+ 
+ The variables are named argc (argument count) and argv (argument vector) by convention, but they can be given any valid identifier: int main(int num_args, char** arg_strings) is equally valid.
+
+ http://stackoverflow.com/questions/3024197/what-does-int-argc-char-argv-mean
+*/
+
+int main(int argc, char *argv[])
 {
+    int distance = 100;
     
-
-    int age = 10;
-    int height = 72;
-
-    printf("I am %d years old.\n", age);
-    printf("I am %d inches tall.\n", height);
-
-    char cat = 'c';
-    float waist = 32.6;
-    
-    printf("The word Cat begins with %c.\n", cat);
-    printf("My waist is %f", waist);
+    // call the int value with %d
+    printf("You are %d miles away.\n", distance);
     
     return 0;
 }
-
-/*
-Reading Material:
- 
-http://www.codingunit.com/printf-format-specifiers-format-conversions-and-formatted-output
-
-
-*/
