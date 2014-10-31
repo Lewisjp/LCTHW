ff5743d228765ed791846f1400ee51f60b96cd73
LewisJPaul <lewis.jp@gmail.com>
2014-10-31 03:16:40 -0400

    Exercise 7: More Variables, Some Math

--- LCTHW/LCTHW/main.c
+++ LCTHW/LCTHW/main.c
@@ -7,6 +7,7 @@
  Quick exercises
  http://c.learncodethehardway.org/
  
+ %e prints in scientific notation
  
 */
 
@@ -14,28 +15,29 @@
 
 int main(int argc, char *argv[])
 {
-    int distance = 100;
-
-    printf("You are %d miles away.\n",distance);
-
-    float power = 2.345f;
-    double super_power = 56789.432;
+    int bugs = 100;
+    double bug_rate = 1.2;
     
-    printf("You have %f levels of powers.\n", power);
-    printf("You have %f awesome super powers.\n", super_power);
+    printf("You have %d bugs at the imaginary rate of %f.\n", bugs, bug_rate);
     
-    char initial = 'A';
-    // This char array is a string
-    // Unlike OOP, C is based on pointers
-    char first_name[] = "Zed";  // Print out char arrary (String) with %s
-    char last_name[] = "Shaw";
+    long universe_of_defects = 1L * 1024L * 1024L * 1024L;
+    printf("The entire universe has %ld bugs.\n", universe_of_defects);
     
-    printf("I have an initial %c.\n", initial);
-    printf("I have a first name %s.\n", first_name);
-    printf("I have a last name %s.\n", last_name);
-
-    printf("My whole name is %s %c. %s.\n", first_name, initial, last_name);
-
+    double expected_bugs = bugs * bug_rate;
+    printf("You are expected to have %f bugs.\n", expected_bugs);
+    
+    double part_of_universe = expected_bugs / universe_of_defects;
+    printf("THis is only a %e portion of the universe.\n", part_of_universe);
+    
+    // '\0' creates the number 0
+    char nul_byte = '\0';
+    int care_percentage = bugs * nul_byte;
+    
+    // two % are used after %d (that displayes the double variable),
+    // %% prints out a single '%'
+    printf("Which means you should care %d%%.\n", care_percentage);
+    
     return 0;
+    
     
 }
