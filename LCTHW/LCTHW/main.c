8c676527a7dab7fae36f131cef88f91b6c7e244b
LewisJPaul <lewis.jp@gmail.com>
2014-11-05 22:13:11 -0500

    Exercise 8: Sizes And Arrays

--- LCTHW/LCTHW/main.c
+++ LCTHW/LCTHW/main.c
@@ -1,43 +1,50 @@
 /*
  Quick review of C
  
- Covers Exercises 1 & 2
- http://www.codingunit.com/printf-format-specifiers-format-conversions-and-formatted-output
- 
  Quick exercises
  http://c.learncodethehardway.org/
  
  %e prints in scientific notation
  
+ '\0' creates the number 0
+ char nul_byte = '\0';
+
+ C treats strings as arrays of bytes, only the printing function knows there's a difference.
+ 
 */
+
+
 
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-    int bugs = 100;
-    double bug_rate = 1.2;
+    int areas[] = { 10,12,13,14,20 };
+    // 5 ints in this arrays
+    // The areas array has 5 integers in it, so it makes sense that my computer requires 20 bytes to store it.
+
     
-    printf("You have %d bugs at the imaginary rate of %f.\n", bugs, bug_rate);
+    char name[] = "Zed";
+    char full_name[]= { 'Z', 'e', 'd', ' ', 'S', 'h', 'a', 'w', '\0'};
     
-    long universe_of_defects = 1L * 1024L * 1024L * 1024L;
-    printf("The entire universe has %ld bugs.\n", universe_of_defects);
+    // WARNING:  On some systems you may have to change the %ld in this code to a %u
+    // since it will use unsigned ints
     
-    double expected_bugs = bugs * bug_rate;
-    printf("You are expected to have %f bugs.\n", expected_bugs);
+    printf("The size of an int: %ld\n", sizeof(int));
+    printf("The size of areas (int[]): %ld\n", sizeof(areas));
     
-    double part_of_universe = expected_bugs / universe_of_defects;
-    printf("THis is only a %e portion of the universe.\n", part_of_universe);
+    printf("The number of ints in areas: %ld\n", sizeof(areas)/ sizeof(int));
+    printf("The first area is %d, the 2nd %d\n", areas[0], areas[1]);
     
-    // '\0' creates the number 0
-    char nul_byte = '\0';
-    int care_percentage = bugs * nul_byte;
+    printf("The size of a char: %ld\n", sizeof(char));
+    printf("The size of name (char[]): %ld\n", sizeof(name));
     
-    // two % are used after %d (that displayes the double variable),
-    // %% prints out a single '%'
-    printf("Which means you should care %d%%.\n", care_percentage);
+    printf("The number of chars: %ld\n", sizeof(name)/sizeof(char));
+    printf("The size of full name (char[]): %ld\n", sizeof(full_name));
+    
+    printf("The number of chars: %ld\n", sizeof(full_name)/ sizeof(char));
+    printf("name = \"%s\" and full_name = \"%s\"\n", name, full_name);
     
     return 0;
-    
     
 }
